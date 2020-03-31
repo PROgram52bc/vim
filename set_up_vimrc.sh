56a1b97ac24556c6b6e78dc037f95573414e79a6
David Deng <daviddenghaotian@163.com>
2020-03-31 01:29:06 -0400

    parameterized create_link in set_up_vimrc.sh, create link for snippet folder

--- set_up_vimrc.sh
+++ set_up_vimrc.sh
@@ -5,18 +5,21 @@ if [ ! -f "$DIR/.vimrc" ]; then
 	return -1
 fi
 
-# Create a link to ~/.vimrc
+# Create a link
 create_link() {
-	echo "Creating a link of $DIR/.vimrc to $HOME..."
-	ln -s $DIR/.vimrc $HOME/.vimrc && echo "done!" || echo "Error occurred when trying to create symbolic link" 
+	echo "Creating a link of $1 to $2..."
+	ln -s "$1" "$2" && echo "done!" || echo "Error occurred when trying to create symbolic link" 
 }
 
-if [ ! -e $HOME/.vimrc -a ! -L $HOME/.vimrc ]; then # if vimrc does not exist (the second condition makes sure it is not a broken link) 
-	create_link
-elif [ -L $HOME/.vimrc -a "$(readlink $HOME/.vimrc)" -ef .vimrc ]; then # if vimrc exists and is a link to the current directory
+if [ -L $HOME/.vimrc -a "$(readlink $HOME/.vimrc)" -ef $DIR/.vimrc ]; then # if vimrc exists and is a link to the current directory
 	#: # do nothing
 	echo "$HOME/.vimrc already points to the .vimrc in the current directory"
-else # if vimrc exists and is not pointing to the current directory
+elif [ ! -e $HOME/.vimrc ]; then # if vimrc does not exist
+	if [ -L $HOME/.vimrc ]; then # if broken link, remove
+		rm $HOME/.vimrc && echo "removed broken link $HOME/.vimrc"
+	fi
+	create_link $DIR/.vimrc $HOME/.vimrc
+elif [ -f $HOME/.vimrc -o -L $HOME/.vimrc ]; then # if vimrc exists as a file or a link to some other file
 	while true; do
 		echo ".vimrc already exists in $HOME, to continue, enter one option: "
 		echo "r) replace with a symlink to the .vimrc in current directory"
@@ -28,7 +31,7 @@ else # if vimrc exists and is not pointing to the current directory
 		case $answer in
 			r)
 				rm $HOME/.vimrc
-				create_link
+				create_link $DIR/.vimrc $HOME/.vimrc
 				break
 				;;
 			c)
@@ -45,6 +48,8 @@ else # if vimrc exists and is not pointing to the current directory
 				;;
 		esac
 	done
+else
+	echo "Undetected file type $HOME/.vimrc, symlink not created"
 fi
 
 # Install Vundle
@@ -80,12 +85,29 @@ fi
 
 # temporary solution: install prettier (should be done with vim plugin manager's post-install hook)
 PRETTIERPATH=$HOME/.vim/bundle/vim-prettier
-if [ ! -d ${PRETTIERPATH} ]; then # if prettier not installed
+if [ ! -d "${PRETTIERPATH}" ]; then # if prettier not installed
 	echo "Prettier not installed (install with Vundle by executing :PluginInstall in vim)"
-elif [ ! -d ${PRETTIERPATH}/node_modules ]; then # if prettier installed but not built
+elif [ ! -d "${PRETTIERPATH}/node_modules" ]; then # if prettier installed but not built
 	cd $PRETTIERPATH
 	echo "Building prettier..."
 	npm install 1>/dev/null 2>&1 && echo "Prettier built in $PRETTIERPATH" || "Failed to install prettier"
 else
 	echo "Prettier already built."
+fi
+
+# Set up UltiSnips
+# TODO: Add more logic to automatically override when the folder is empty, or is a broken link
+ULTISNIPSPATH=$HOME/.vim/UltiSnips
+
+if [ -d "${DIR}/UltiSnips" ]; then # if snippet folder exists in current directory
+	if [ -L ${ULTISNIPSPATH} -a "$(readlink ${ULTISNIPSPATH})" -ef $DIR/UltiSnips ]; then # if there is already a link pointing to the folder in current directory
+		echo "$ULTISNIPSPATH already points to the current directory"
+	elif [ -d ${ULTISNIPSPATH} -o -L ${ULTISNIPSPATH} ]; then # if the folder exists, or is a link to other directory (or a broken link), ask for conflict resolution
+		echo "Directory ${ULTISNIPSPATH} already exists, symlink not created. Please check and merge any wanted snippet files."
+	 	echo "You can rerun this script again with ${ULTISNIPSPATH} deleted"
+	elif [ ! -e "$ULTISNIPSPATH" ]; then # if does not exist at all
+		create_link $DIR/UltiSnips $ULTISNIPSPATH
+	else # if exists in any other form
+		echo "Undetected file type: ${ULTISNIPSPATH}, symlink not created"
+	fi
 fi
