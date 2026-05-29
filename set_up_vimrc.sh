a05594a8c39eea821274113c99432653a9cde044
David Deng <htd@amazon.com>
2026-05-29 01:37:32 -0700

    quoting and portability sweep in set_up_vimrc.sh
    - Quote $HOME, $DIR, $ULTISNIPSPATH, $PLUGPATH everywhere they're
      passed to test/[, rm, cp, ln, diff, mkdir — works on paths with
      spaces.
    - Replace deprecated -a/-o test operators with && and ||.
    - Drop create_link's realpath of the destination (the dest doesn't
      exist yet, so abs_path returned garbage and the link landed in
      the wrong place).
    - Use 'curl ... && ...' / 'if ln -s' pattern instead of '$? -eq 0'.
    - exit 1 instead of exit -1 (POSIX).
    - Existence check via [ -e ] instead of ls.

--- set_up_vimrc.sh
+++ set_up_vimrc.sh
@@ -7,31 +7,30 @@ realpath() {
 DIR=$(dirname "$(realpath "$0")") # get current directory
 if [ ! -f "$DIR/.vimrc" ]; then
 	echo ".vimrc not detected in $DIR, terminating script"
-	exit -1
+	exit 1
 fi
 
 # Create a link
 create_link() {
 	echo "Creating a link of $1 to $2..."
-	local src=`realpath "$1"`
-	local dest=`realpath "$2"`
-	ln -s "$src" "$dest"
-	if [ $? -eq 0 ]; then
+	local src
+	src=$(realpath "$1")
+	if ln -s "$src" "$2"; then
 		echo "done!"
 	else
-		echo "Error occurred when trying to create symbolic link" 
+		echo "Error occurred when trying to create symbolic link"
 	fi
 }
 
-if [ -L $HOME/.vimrc -a "$(readlink $HOME/.vimrc)" -ef $DIR/.vimrc ]; then # if vimrc exists and is a link to the current directory
+if [ -L "$HOME/.vimrc" ] && [ "$(readlink "$HOME/.vimrc")" -ef "$DIR/.vimrc" ]; then # if vimrc exists and is a link to the current directory
 	#: # do nothing
 	echo "$HOME/.vimrc already points to the .vimrc in the current directory"
-elif [ ! -e $HOME/.vimrc ]; then # if vimrc does not exist
-	if [ -L $HOME/.vimrc ]; then # if broken link, remove
-		rm $HOME/.vimrc && echo "removed broken link $HOME/.vimrc"
+elif [ ! -e "$HOME/.vimrc" ]; then # if vimrc does not exist
+	if [ -L "$HOME/.vimrc" ]; then # if broken link, remove
+		rm "$HOME/.vimrc" && echo "removed broken link $HOME/.vimrc"
 	fi
-	create_link $DIR/.vimrc $HOME/.vimrc
-elif [ -f $HOME/.vimrc -o -L $HOME/.vimrc ]; then # if vimrc exists as a file or a link to some other file
+	create_link "$DIR/.vimrc" "$HOME/.vimrc"
+elif [ -f "$HOME/.vimrc" ] || [ -L "$HOME/.vimrc" ]; then # if vimrc exists as a file or a link to some other file
 	while true; do
 		echo ".vimrc already exists in $HOME, to continue, enter one option: "
 		echo "r) replace with a symlink to the .vimrc in current directory"
@@ -42,17 +41,17 @@ elif [ -f $HOME/.vimrc -o -L $HOME/.vimrc ]; then # if vimrc exists as a file or
 		echo
 		case $answer in
 			r)
-				rm $HOME/.vimrc
-				create_link $DIR/.vimrc $HOME/.vimrc
+				rm "$HOME/.vimrc"
+				create_link "$DIR/.vimrc" "$HOME/.vimrc"
 				break
 				;;
 			c)
-				rm $HOME/.vimrc
-				cp $DIR/.vimrc $HOME/.vimrc && echo "replaced with a copy of .vimrc in current directory"
+				rm "$HOME/.vimrc"
+				cp "$DIR/.vimrc" "$HOME/.vimrc" && echo "replaced with a copy of .vimrc in current directory"
 				break
 				;;
 			s)
-				diff -u $HOME/.vimrc $DIR/.vimrc | less
+				diff -u "$HOME/.vimrc" "$DIR/.vimrc" | less
 				echo
 				;;
 			*)
@@ -67,8 +66,7 @@ fi
 # Install Plug
 PLUGPATH=$HOME/.vim/autoload/plug.vim
 PLUGLINK=https://github.com/junegunn/vim-plug.git
-ls $PLUGPATH 1>/dev/null 2>&1
-if [ $? -eq 0 ]; then # if Plug installed
+if [ -e "$PLUGPATH" ]; then
 	echo "Plug detected in $PLUGPATH."
 else
 	while true; do
@@ -80,12 +78,11 @@ else
 		echo
 		case $answer in
 			y)
-				mkdir -p ${PLUGPATH%/*}
+				mkdir -p "${PLUGPATH%/*}"
 				echo "Start Installing Plug"
 				echo "cloning from $PLUGLINK..."
-				curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-				if [ $? -eq 0 ]; then
-					echo "Plug installed in $PLUGPATH" 
+				if curl -fLo "$PLUGPATH" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim; then
+					echo "Plug installed in $PLUGPATH"
 					vim +PlugInstall +qall
 					echo "Installed vim plugin"
 				else
@@ -106,16 +103,16 @@ fi
 # TODO: Add more logic to automatically override when the folder is empty, or is a broken link
 ULTISNIPSPATH=$HOME/.vim/UltiSnips
 
-if [ -d "${DIR}/UltiSnips" ]; then # if snippet folder exists in current directory
-	if [ -L ${ULTISNIPSPATH} -a "$(readlink ${ULTISNIPSPATH})" -ef $DIR/UltiSnips ]; then # if there is already a link pointing to the folder in current directory
+if [ -d "$DIR/UltiSnips" ]; then # if snippet folder exists in current directory
+	if [ -L "$ULTISNIPSPATH" ] && [ "$(readlink "$ULTISNIPSPATH")" -ef "$DIR/UltiSnips" ]; then # if there is already a link pointing to the folder in current directory
 		echo "$ULTISNIPSPATH already points to the current directory"
-	elif [ -d ${ULTISNIPSPATH} -o -L ${ULTISNIPSPATH} ]; then # if the folder exists, or is a link to other directory (or a broken link), ask for conflict resolution
-		echo "Directory ${ULTISNIPSPATH} already exists, symlink not created. Please check and merge any wanted snippet files."
-	 	echo "You can rerun this script again with ${ULTISNIPSPATH} deleted"
+	elif [ -d "$ULTISNIPSPATH" ] || [ -L "$ULTISNIPSPATH" ]; then # if the folder exists, or is a link to other directory (or a broken link), ask for conflict resolution
+		echo "Directory $ULTISNIPSPATH already exists, symlink not created. Please check and merge any wanted snippet files."
+		echo "You can rerun this script again with $ULTISNIPSPATH deleted"
 	elif [ ! -e "$ULTISNIPSPATH" ]; then # if does not exist at all
-		create_link $DIR/UltiSnips $ULTISNIPSPATH
+		create_link "$DIR/UltiSnips" "$ULTISNIPSPATH"
 	else # if exists in any other form
-		echo "Undetected file type: ${ULTISNIPSPATH}, symlink not created"
+		echo "Undetected file type: $ULTISNIPSPATH, symlink not created"
 	fi
 fi
 
@@ -128,10 +125,10 @@ while true; do
 	echo
 	case $answer in
 		y)
-			if [ ! -f ~/.config/nvim/init.vim ]; then
-				mkdir -p ~/.config/nvim
+			if [ ! -f "$HOME/.config/nvim/init.vim" ]; then
+				mkdir -p "$HOME/.config/nvim"
 				echo "Creating ~/.config/nvim/init.vim..."
-				cat > ~/.config/nvim/init.vim << 'HERE'
+				cat > "$HOME/.config/nvim/init.vim" << 'HERE'
 " Make Neovim see Vim runtime and packages
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
@@ -144,9 +141,9 @@ HERE
 				echo "~/.config/nvim/init.vim already exists. Remove it and rerun this script to set it up again."
 			fi
 			echo "Creating symlinks of lua plugins into ~/.config/nvim/plugin"
-			mkdir -p ~/.config/nvim/plugin
+			mkdir -p "$HOME/.config/nvim/plugin"
 			for f in "$DIR"/nv/lua/*.lua; do
-				dest=~/.config/nvim/plugin/$(basename "$f")
+				dest="$HOME/.config/nvim/plugin/$(basename "$f")"
 				if [ -L "$dest" ] && [ "$(readlink "$dest")" -ef "$f" ]; then
 					echo "$dest already points to $f"
 					continue
